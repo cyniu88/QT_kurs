32dc36df16fe205064eaf365c3d83ff0fc28ef5b
cyniu88 <bazz5@wp.pl>
2016-06-13 21:06:44 +0200

    zamykanie przyciskiem i inne

--- build-iDom-cli/ui_idom_client.h
+++ build-iDom-cli/ui_idom_client.h
@@ -16,8 +16,10 @@
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QTabWidget>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
@@ -29,25 +31,34 @@ class Ui_iDom_Client
 public:
     QWidget *centralWidget;
     QGridLayout *gridLayout;
-    QGridLayout *gridLayout_2;
-    QLabel *LOGO;
-    QVBoxLayout *verticalLayout_3;
-    QPushButton *pushButton_2;
-    QPushButton *pushButton;
     QTabWidget *tabWidget;
     QWidget *tab;
     QPushButton *pushButton_3;
-    QPushButton *icona;
     QWidget *tab_2;
+    QGridLayout *gridLayout_2;
+    QLabel *LOGO;
+    QSpacerItem *horizontalSpacer_2;
+    QVBoxLayout *verticalLayout_3;
+    QPushButton *EXIT_button;
+    QPushButton *disconnectButton;
+    QVBoxLayout *verticalLayout;
+    QPushButton *playButton;
+    QPushButton *stopButton;
+    QVBoxLayout *verticalLayout_2;
+    QVBoxLayout *verticalLayout_4;
+    QLineEdit *titleTXT;
+    QLineEdit *volumeTXT;
 
     void setupUi(QMainWindow *iDom_Client)
     {
         if (iDom_Client->objectName().isEmpty())
             iDom_Client->setObjectName(QStringLiteral("iDom_Client"));
-        iDom_Client->resize(480, 800);
+        iDom_Client->resize(1280, 800);
         iDom_Client->setMinimumSize(QSize(480, 800));
         iDom_Client->setMouseTracking(true);
         iDom_Client->setFocusPolicy(Qt::StrongFocus);
+        iDom_Client->setWindowOpacity(3);
+        iDom_Client->setLayoutDirection(Qt::LeftToRight);
         iDom_Client->setAutoFillBackground(true);
         iDom_Client->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
         iDom_Client->setDockOptions(QMainWindow::AllowTabbedDocks|QMainWindow::AnimatedDocks|QMainWindow::VerticalTabs);
@@ -58,34 +69,6 @@ public:
         gridLayout->setSpacing(6);
         gridLayout->setContentsMargins(11, 11, 11, 11);
         gridLayout->setObjectName(QStringLiteral("gridLayout"));
-        gridLayout_2 = new QGridLayout();
-        gridLayout_2->setSpacing(6);
-        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
-        LOGO = new QLabel(centralWidget);
-        LOGO->setObjectName(QStringLiteral("LOGO"));
-        LOGO->setPixmap(QPixmap(QString::fromUtf8("../build-iDom-cli/f/logo.png")));
-
-        gridLayout_2->addWidget(LOGO, 0, 0, 1, 1);
-
-        verticalLayout_3 = new QVBoxLayout();
-        verticalLayout_3->setSpacing(1);
-        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
-        pushButton_2 = new QPushButton(centralWidget);
-        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
-
-        verticalLayout_3->addWidget(pushButton_2);
-
-        pushButton = new QPushButton(centralWidget);
-        pushButton->setObjectName(QStringLiteral("pushButton"));
-
-        verticalLayout_3->addWidget(pushButton);
-
-
-        gridLayout_2->addLayout(verticalLayout_3, 0, 1, 1, 1);
-
-
-        gridLayout->addLayout(gridLayout_2, 0, 0, 1, 1);
-
         tabWidget = new QTabWidget(centralWidget);
         tabWidget->setObjectName(QStringLiteral("tabWidget"));
         tabWidget->setMinimumSize(QSize(0, 350));
@@ -98,26 +81,206 @@ public:
         pushButton_3 = new QPushButton(tab);
         pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
         pushButton_3->setGeometry(QRect(310, 610, 93, 28));
-        icona = new QPushButton(tab);
-        icona->setObjectName(QStringLiteral("icona"));
-        icona->setGeometry(QRect(110, 100, 221, 111));
-        icona->setMaximumSize(QSize(16777215, 16777211));
         QIcon icon;
         icon.addFile(QStringLiteral("../build-iDom-cli/f/logo.png"), QSize(), QIcon::Normal, QIcon::Off);
-        icona->setIcon(icon);
-        icona->setIconSize(QSize(150, 125));
         tabWidget->addTab(tab, icon, QString());
         pushButton_3->raise();
-        icona->raise();
         tab_2 = new QWidget();
         tab_2->setObjectName(QStringLiteral("tab_2"));
         tabWidget->addTab(tab_2, QString());
 
         gridLayout->addWidget(tabWidget, 1, 0, 1, 1);
 
+        gridLayout_2 = new QGridLayout();
+        gridLayout_2->setSpacing(6);
+        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        LOGO = new QLabel(centralWidget);
+        LOGO->setObjectName(QStringLiteral("LOGO"));
+        LOGO->setMinimumSize(QSize(200, 150));
+        LOGO->setSizeIncrement(QSize(0, 0));
+        LOGO->setLayoutDirection(Qt::LeftToRight);
+        LOGO->setFrameShape(QFrame::NoFrame);
+        LOGO->setFrameShadow(QFrame::Sunken);
+        LOGO->setPixmap(QPixmap(QString::fromUtf8("../build-iDom-cli/f/logo.png")));
+        LOGO->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter);
+
+        gridLayout_2->addWidget(LOGO, 1, 0, 1, 1);
+
+        horizontalSpacer_2 = new QSpacerItem(10, 10, QSizePolicy::Preferred, QSizePolicy::Minimum);
+
+        gridLayout_2->addItem(horizontalSpacer_2, 1, 3, 1, 1);
+
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setSpacing(0);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        EXIT_button = new QPushButton(centralWidget);
+        EXIT_button->setObjectName(QStringLiteral("EXIT_button"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(EXIT_button->sizePolicy().hasHeightForWidth());
+        EXIT_button->setSizePolicy(sizePolicy);
+        EXIT_button->setMaximumSize(QSize(250, 16777215));
+        QFont font;
+        font.setFamily(QStringLiteral("Impact"));
+        font.setPointSize(10);
+        font.setItalic(true);
+        EXIT_button->setFont(font);
+
+        verticalLayout_3->addWidget(EXIT_button);
+
+        disconnectButton = new QPushButton(centralWidget);
+        disconnectButton->setObjectName(QStringLiteral("disconnectButton"));
+        sizePolicy.setHeightForWidth(disconnectButton->sizePolicy().hasHeightForWidth());
+        disconnectButton->setSizePolicy(sizePolicy);
+        disconnectButton->setMaximumSize(QSize(250, 16777215));
+        QPalette palette;
+        QBrush brush(QColor(170, 255, 0, 255));
+        brush.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Link, brush);
+        QBrush brush1(QColor(0, 0, 255, 255));
+        brush1.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Inactive, QPalette::Link, brush1);
+        palette.setBrush(QPalette::Disabled, QPalette::Link, brush1);
+        disconnectButton->setPalette(palette);
+        disconnectButton->setFont(font);
+        disconnectButton->setLayoutDirection(Qt::RightToLeft);
+
+        verticalLayout_3->addWidget(disconnectButton);
+
+
+        gridLayout_2->addLayout(verticalLayout_3, 1, 4, 1, 1);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(6);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        playButton = new QPushButton(centralWidget);
+        playButton->setObjectName(QStringLiteral("playButton"));
+        sizePolicy.setHeightForWidth(playButton->sizePolicy().hasHeightForWidth());
+        playButton->setSizePolicy(sizePolicy);
+        playButton->setMaximumSize(QSize(250, 16777215));
+        playButton->setFont(font);
+
+        verticalLayout->addWidget(playButton);
+
+        stopButton = new QPushButton(centralWidget);
+        stopButton->setObjectName(QStringLiteral("stopButton"));
+        sizePolicy.setHeightForWidth(stopButton->sizePolicy().hasHeightForWidth());
+        stopButton->setSizePolicy(sizePolicy);
+        stopButton->setMaximumSize(QSize(250, 16777215));
+        stopButton->setFont(font);
+
+        verticalLayout->addWidget(stopButton);
+
+
+        gridLayout_2->addLayout(verticalLayout, 1, 2, 1, 1);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+
+        gridLayout_2->addLayout(verticalLayout_2, 0, 1, 1, 1);
+
+        verticalLayout_4 = new QVBoxLayout();
+        verticalLayout_4->setSpacing(6);
+        verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        titleTXT = new QLineEdit(centralWidget);
+        titleTXT->setObjectName(QStringLiteral("titleTXT"));
+        QSizePolicy sizePolicy1(QSizePolicy::Expanding, QSizePolicy::Preferred);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(titleTXT->sizePolicy().hasHeightForWidth());
+        titleTXT->setSizePolicy(sizePolicy1);
+        QPalette palette1;
+        QBrush brush2(QColor(0, 0, 0, 255));
+        brush2.setStyle(Qt::SolidPattern);
+        palette1.setBrush(QPalette::Active, QPalette::WindowText, brush2);
+        QBrush brush3(QColor(85, 255, 0, 255));
+        brush3.setStyle(Qt::SolidPattern);
+        palette1.setBrush(QPalette::Active, QPalette::Button, brush3);
+        QBrush brush4(QColor(170, 255, 127, 255));
+        brush4.setStyle(Qt::SolidPattern);
+        palette1.setBrush(QPalette::Active, QPalette::Light, brush4);
+        QBrush brush5(QColor(127, 255, 63, 255));
+        brush5.setStyle(Qt::SolidPattern);
+        palette1.setBrush(QPalette::Active, QPalette::Midlight, brush5);
+        QBrush brush6(QColor(42, 127, 0, 255));
+        brush6.setStyle(Qt::SolidPattern);
+        palette1.setBrush(QPalette::Active, QPalette::Dark, brush6);
+        QBrush brush7(QColor(56, 170, 0, 255));
+        brush7.setStyle(Qt::SolidPattern);
+        palette1.setBrush(QPalette::Active, QPalette::Mid, brush7);
+        palette1.setBrush(QPalette::Active, QPalette::Text, brush2);
+        QBrush brush8(QColor(255, 255, 255, 255));
+        brush8.setStyle(Qt::SolidPattern);
+        palette1.setBrush(QPalette::Active, QPalette::BrightText, brush8);
+        palette1.setBrush(QPalette::Active, QPalette::ButtonText, brush2);
+        palette1.setBrush(QPalette::Active, QPalette::Base, brush8);
+        palette1.setBrush(QPalette::Active, QPalette::Window, brush3);
+        palette1.setBrush(QPalette::Active, QPalette::Shadow, brush2);
+        palette1.setBrush(QPalette::Active, QPalette::AlternateBase, brush4);
+        QBrush brush9(QColor(255, 255, 220, 255));
+        brush9.setStyle(Qt::SolidPattern);
+        palette1.setBrush(QPalette::Active, QPalette::ToolTipBase, brush9);
+        palette1.setBrush(QPalette::Active, QPalette::ToolTipText, brush2);
+        palette1.setBrush(QPalette::Inactive, QPalette::WindowText, brush2);
+        palette1.setBrush(QPalette::Inactive, QPalette::Button, brush3);
+        palette1.setBrush(QPalette::Inactive, QPalette::Light, brush4);
+        palette1.setBrush(QPalette::Inactive, QPalette::Midlight, brush5);
+        palette1.setBrush(QPalette::Inactive, QPalette::Dark, brush6);
+        palette1.setBrush(QPalette::Inactive, QPalette::Mid, brush7);
+        palette1.setBrush(QPalette::Inactive, QPalette::Text, brush2);
+        palette1.setBrush(QPalette::Inactive, QPalette::BrightText, brush8);
+        palette1.setBrush(QPalette::Inactive, QPalette::ButtonText, brush2);
+        palette1.setBrush(QPalette::Inactive, QPalette::Base, brush8);
+        palette1.setBrush(QPalette::Inactive, QPalette::Window, brush3);
+        palette1.setBrush(QPalette::Inactive, QPalette::Shadow, brush2);
+        palette1.setBrush(QPalette::Inactive, QPalette::AlternateBase, brush4);
+        palette1.setBrush(QPalette::Inactive, QPalette::ToolTipBase, brush9);
+        palette1.setBrush(QPalette::Inactive, QPalette::ToolTipText, brush2);
+        palette1.setBrush(QPalette::Disabled, QPalette::WindowText, brush6);
+        palette1.setBrush(QPalette::Disabled, QPalette::Button, brush3);
+        palette1.setBrush(QPalette::Disabled, QPalette::Light, brush4);
+        palette1.setBrush(QPalette::Disabled, QPalette::Midlight, brush5);
+        palette1.setBrush(QPalette::Disabled, QPalette::Dark, brush6);
+        palette1.setBrush(QPalette::Disabled, QPalette::Mid, brush7);
+        palette1.setBrush(QPalette::Disabled, QPalette::Text, brush6);
+        palette1.setBrush(QPalette::Disabled, QPalette::BrightText, brush8);
+        palette1.setBrush(QPalette::Disabled, QPalette::ButtonText, brush6);
+        palette1.setBrush(QPalette::Disabled, QPalette::Base, brush3);
+        palette1.setBrush(QPalette::Disabled, QPalette::Window, brush3);
+        palette1.setBrush(QPalette::Disabled, QPalette::Shadow, brush2);
+        palette1.setBrush(QPalette::Disabled, QPalette::AlternateBase, brush3);
+        palette1.setBrush(QPalette::Disabled, QPalette::ToolTipBase, brush9);
+        palette1.setBrush(QPalette::Disabled, QPalette::ToolTipText, brush2);
+        titleTXT->setPalette(palette1);
+        QFont font1;
+        font1.setPointSize(12);
+        titleTXT->setFont(font1);
+        titleTXT->setReadOnly(true);
+        titleTXT->setClearButtonEnabled(false);
+
+        verticalLayout_4->addWidget(titleTXT);
+
+        volumeTXT = new QLineEdit(centralWidget);
+        volumeTXT->setObjectName(QStringLiteral("volumeTXT"));
+        sizePolicy.setHeightForWidth(volumeTXT->sizePolicy().hasHeightForWidth());
+        volumeTXT->setSizePolicy(sizePolicy);
+        volumeTXT->setFont(font1);
+        volumeTXT->setReadOnly(true);
+
+        verticalLayout_4->addWidget(volumeTXT);
+
+
+        gridLayout_2->addLayout(verticalLayout_4, 1, 1, 1, 1);
+
+
+        gridLayout->addLayout(gridLayout_2, 0, 0, 1, 1);
+
         iDom_Client->setCentralWidget(centralWidget);
 
         retranslateUi(iDom_Client);
+        QObject::connect(EXIT_button, SIGNAL(clicked()), iDom_Client, SLOT(close()));
 
         QMetaObject::connectSlotsByName(iDom_Client);
     } // setupUi
@@ -125,13 +288,16 @@ public:
     void retranslateUi(QMainWindow *iDom_Client)
     {
         iDom_Client->setWindowTitle(QApplication::translate("iDom_Client", "iDom_Client", 0));
-        LOGO->setText(QString());
-        pushButton_2->setText(QApplication::translate("iDom_Client", "PushButton", 0));
-        pushButton->setText(QApplication::translate("iDom_Client", "PushButton", 0));
         pushButton_3->setText(QApplication::translate("iDom_Client", "PushButton", 0));
-        icona->setText(QApplication::translate("iDom_Client", "OK", 0));
         tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("iDom_Client", "Tab 1", 0));
         tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("iDom_Client", "Tab 2", 0));
+        LOGO->setText(QString());
+        EXIT_button->setText(QApplication::translate("iDom_Client", "EXIT", 0));
+        disconnectButton->setText(QApplication::translate("iDom_Client", "DISCONNECT", 0));
+        playButton->setText(QApplication::translate("iDom_Client", "PLAY", 0));
+        stopButton->setText(QApplication::translate("iDom_Client", "STOP", 0));
+        titleTXT->setInputMask(QApplication::translate("iDom_Client", "reerewreerereererfevfvev", 0));
+        volumeTXT->setInputMask(QApplication::translate("iDom_Client", "34254545345", 0));
     } // retranslateUi
 
 };
--- iDom_Client/idom_client.cpp
+++ iDom_Client/idom_client.cpp
@@ -35,7 +35,7 @@ iDom_Client::iDom_Client(iDom_CONFIG *config, QWidget *parent) :
         qDebug("udalo sie android");
     }
     ui->LOGO->setPixmap(pix);
-    ui->icona->setIcon(pix);
+    //ui->icona->setIcon(pix);
 }
 
 iDom_Client::~iDom_Client()
