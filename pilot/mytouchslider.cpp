62a3559c64e1dda225d13e915c5431a4382035fe
cyniu88 <bazz5@wp.pl>
2016-11-15 08:04:32 +0100

    poczatek prac na touch sliderem

--- pilot/mytouchslider.cpp
+++ pilot/mytouchslider.cpp
@@ -4,10 +4,20 @@
 myTouchSlider::myTouchSlider(QObject *parent)
 {
     this->setAttribute(Qt::WA_AcceptTouchEvents);
+    //this->grabGesture(Qt::TapAndHoldGesture);
 }
 
 bool myTouchSlider::event(QEvent *e)
 {
     qDebug() << "EVENT =)" << e->type();
-    QSlider::event(e);
+    switch (e->type()){
+    case QEvent::TouchBegin:
+        qDebug() << "start dotykania";
+        break;
+    case QEvent::TouchEnd:
+        qDebug() << " KONIEC dotykania";
+        break;
+    }
+
+    return QSlider::event(e);
 }
