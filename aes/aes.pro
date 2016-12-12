dbcde9e1ab8b7b3545ea35e0f1409dd566beac99
Krzysztof Druciak <krzysztof.druciak@ericsson.com>
2016-12-12 08:22:03 +0100

    aes

---
+++ aes/aes.pro
@@ -0,0 +1,10 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += main.cpp \
+    cipher.cpp
+
+HEADERS += \
+    cipher.h
---
+++ aes/main.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//#include "cipher.h"
+
+//using namespace std;
+
+//int main()
+//{
+//    string data = "nic z tego nie wydzie  bo  jajo zniose ";
+//    Cipher::encrypt(data);
+//    cout << "Encrypted: " << data << endl;
+//    Cipher::decrypt(data);
+
+
+//    cout << "Decrypted: " << data<<"|"<<std::endl;
+//    return 0;
+//}
