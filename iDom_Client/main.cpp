30487a02c759a5d4f87866d16767b28d20a526fa
Krzysztof Druciak <krzysztof.druciak@ericsson.com>
2017-02-20 07:54:41 +0100

    ucuniecie ikony w kodzie + nowa wersja exe

--- iDom_Client/main.cpp
+++ iDom_Client/main.cpp
@@ -22,7 +22,6 @@ int main(int argc, char *argv[])
     WorkerIP * worker = new WorkerIP(&config);
     config.worketPTR  = worker;
     QApplication a(argc, argv);
-    a.setWindowIcon( QIcon(":/new/prefix1/iDom_client.ico"));
 
 #ifdef Q_OS_ANDROID
     QtWebView::initialize();
